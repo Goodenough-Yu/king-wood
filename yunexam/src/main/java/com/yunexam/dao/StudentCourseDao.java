614c1c84f7b20b1bb77a05b023033ae6c5ebb72c
韩林岐 <858590598@qq.com>
2019-12-12 14:13:44 +0800

    1

--- yunexam/src/main/java/com/yunexam/dao/StudentCourseDao.java
+++
@@ -1,8 +0,0 @@
-package com.yunexam.dao;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface StudentCourseDao {
-    List<Integer> FindStuCouBysid(int sid) throws SQLException; // 根据sid查找cid
-}
