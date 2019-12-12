d18c80996c60e2253f3c8120640a94d57d249b47
韩林岐 <858590598@qq.com>
2019-12-12 12:38:14 +0800

    用户跳转

--- yunexam/src/main/java/com/yunexam/controller/LoginControl.java
+++ yunexam/src/main/java/com/yunexam/controller/LoginControl.java
@@ -5,6 +5,9 @@ import com.yunexam.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.SQLException;
 
@@ -14,21 +17,60 @@ public class LoginControl {
     @Autowired
     LoginService loginService;
 
+    /**
+     * 进入登录界面
+     * @return
+     */
     @RequestMapping(path = "")
     public String Login(){
-        // 用户登陆 service 验证处理
-        System.out.println("用户登录");
         return "login.html";
     }
 
-    public  String StudentLogin(Student student) throws SQLException {
-        int sid = student.getSid();
-        String spwd = student.getSpwd();
-        if(loginService.StudentLogin(sid,spwd)){
-            return "/template/student";
-        }else {
-            return "login.html";
-        }
+    /**
+     *  检查学生用户名和密码
+     * @param sidStr 用户名
+     * @param spwd 密码
+     * @return 是否存在用户
+     * @throws SQLException
+     */
+    @ResponseBody
+    @RequestMapping(path = "/studentLogin", method = RequestMethod.POST)
+    public  boolean StudentLogin(
+            @RequestParam("username") String sidStr,
+            @RequestParam("password") String spwd) throws SQLException {
+        int sid = Integer.parseInt(sidStr);
+        return loginService.StudentLogin(sid,spwd);
+    }
 
+    /**
+     *  检查教师用户名和密码
+     * @param sidStr 用户名
+     * @param spwd 密码
+     * @return 是否存在用户
+     * @throws SQLException
+     */
+    @ResponseBody
+    @RequestMapping(path = "/teacherLogin", method = RequestMethod.POST)
+    public  boolean teacherLogin(
+            @RequestParam("username") String sidStr,
+            @RequestParam("password") String spwd) throws SQLException {
+        int sid = Integer.parseInt(sidStr);
+        return loginService.TeacherLogin(sid,spwd);
+    }
+
+    /**
+     *  检查管理员用户名和密码
+     * @param sidStr 用户名
+     * @param spwd 密码
+     * @return 是否存在用户
+     * @throws SQLException
+     */
+    @ResponseBody
+    @RequestMapping(path = "/adminLogin", method = RequestMethod.POST)
+    public  boolean AdminLogin(
+            @RequestParam("username") String sidStr,
+            @RequestParam("password") String spwd) throws SQLException {
+        int sid = Integer.parseInt(sidStr);
+        return loginService.AdminLogin(sid,spwd);
     }
 }
