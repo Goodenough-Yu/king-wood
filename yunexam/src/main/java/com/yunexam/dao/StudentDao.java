1bf9d329dc0c24fea357c7546dc029ab6a51d5d6
Goodenough Yu <56597008+Goodenough-Yu@users.noreply.github.com>
2019-12-12 14:07:26 +0800

    1

--- yunexam/src/main/java/com/yunexam/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.yunexam.dao;
-
-import com.yunexam.domain.Student;
-
-import java.sql.SQLException;
-
-public interface StudentDao {
-    Student FindStuBynameAndpwd(int sid, String spwd) throws SQLException; // 根据Sid和Spwd查找该学生
-}
--- yunexam/src/main/java/com/yunexam/service/GradeService.java
+++
@@ -1,9 +0,0 @@
-package com.yunexam.service;
-
-import java.sql.SQLException;
-import java.util.Map;
-
-public interface GradeService {
-    Map<String, Object> getGrade(int sid) throws SQLException; // 根据sid返回grade,course
-    Map<String, Object> getSolution(int gid) throws SQLException; // 根据gid返回solution,question_bank,grade
-}
